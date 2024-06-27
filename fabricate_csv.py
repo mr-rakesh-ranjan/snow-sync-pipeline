71d2a7c6515883fb481efc214633066b1976a5b1
mr-rakesh-ranjan <rakeshranjanhjp@gmail.com>
2024-06-27 17:29:41 +0530

    removed special char

--- fabricate_csv.py
+++ fabricate_csv.py
@@ -9,13 +9,11 @@ def fabricate_csv(df : 'pd.core.frame.DataFrame'):
     for i in range(0, len(df), 1):
         df['text'][i] = BeautifulSoup(df['text'][i], "lxml").text
         df['text'][i] = re.sub(r'\s', ' ', df['text'][i])
-    # del df['Unnamed: 0']
     df.to_csv("serviceNow_cleaned.csv", index=False)
     return df
     
     
 
 df1 = pd.read_csv("./kb_articles.csv", encoding='unicode_escape')
-# print(df1["text"][0])
 new_csv = fabricate_csv(df=df1)
 new_csv.to_csv("serviceNow_cleaned.csv", index=False)
